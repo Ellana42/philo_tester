9a046b347eae08c94e9aa0f6c3a17d195767b081
Ellana42 <mtmrkaploun@gmail.com>
2022-12-05 15:39:31 +0100

    Some state

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+# TODO Add deaths and min eaten count
+# TODO Add interruption for never die
+from collections import Counter
 import os
+from typing import List
 
 from termcolor import colored
 
@@ -54,7 +58,7 @@ args = [
     # "2 800 200 200",
 ]
 args2 = [
-    "5 800 200 200 9",
+    "5 800 200 200 2",
     "5 0 200 200",
     "5 800 0 200 9",
     "5 800 200 0 9",
@@ -75,10 +79,84 @@ args3 = [
 ]
 
 
+class Action:
+    def __init__(self, time, philo, action):
+        self.time = time
+        self.philo = philo
+        self.action = action
+
+    @staticmethod
+    def line_to_action(line_output):
+        words = line_output.split(" ")
+        if len(words) > 3:
+            return (words[0], words[1], words[3])
+        if len(words) == 3:
+            return (words[0], words[1], words[2])
+        return (None, None, None)
+
+
+class Philo:
+    def __init__(self, index: int, actions: List[Action]):
+        self.index = index
+        self.actions = actions
+
+    def get_nb_meals(self):
+        nb_meals = 0
+        for action in self.actions:
+            if action.action == "eating":
+                nb_meals += 1
+        return nb_meals
+
+    def is_dead(self):
+        nb_meals = 0
+        for action in self.actions:
+            if action.action == "died":
+                return 1
+        return 0
+
+
+class Simulation:
+    def __init__(self, actions: List[Action]):
+        self.actions = actions
+        self.philos = self.init_philos()
+
+    def init_philos(self):
+        philos = {}
+        for action in self.actions:
+            philos.setdefault(action.philo, []).append(action)
+        philos_list = []
+        for i in range(len(philos)):
+            philos_list.append(Philo(i, philos[str(i)]))
+        return philos_list
+
+
+def parse_output(output):
+    actions = [Action(**Action.line_to_action(line)) for line in output.split("\n")]
+    actions = {}
+    for line in output.split("\n"):
+        action = line_to_action(line)
+        if action[0] is not None:
+            if action[1] not in actions:
+                actions[action[1]] = []
+            actions[action[1]].append(action[0])
+    return actions
+
+
+def count_min_eat(output):
+    actions = parse_output(output)
+    return Counter(actions["eating"]).most_common()[:-2:-1][0][1]
+
+
 def print_output(v, output):
     # print(output)
     if v == 0:
         print(colored(output, "blue"))
+        print("----------------------")
+        print(colored(f"Min meals : {count_min_eat(output)}", "green"))
+        if "die" in output:
+            print(colored("Death", "red"))
+        else:
+            print(colored("No death", "green"))
     if v == 1:
         if "no leaks are possible" in output:
             print(colored("NO LEAKS", "green"))
